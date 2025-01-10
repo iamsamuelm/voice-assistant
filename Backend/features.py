c5749118949181c961a6381aaa3f39dc5c8ebf6b
Samuel S. Bond <157819567+iamsamuelm@users.noreply.github.com>
2025-01-09 20:26:23 -0500

    new file:   Backend/__pycache__/features.cpython-312.pyc 	modified:   Backend/features.py 	modified:   Backend/main.py 	modified:   Frontend/assets/.DS_Store

--- Backend/features.py
+++ Backend/features.py
@@ -1,6 +1,11 @@
+import os
 from playsound import playsound
 
 # Friday start sound
 def playStartSound():
-    sound_path = "Frontend/assets/audio/soft-startup-sound.mp3"
+    # Dynamically resolve the absolute path to the sound file
+    sound_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),  # Directory of features.py
+        "../Frontend/assets/audio/soft-startup-sound.mp3"
+    )
     playsound(sound_path)
--- Backend/main.py
+++ Backend/main.py
@@ -1,14 +1,10 @@
 import os
 import eel
 import subprocess
-import threading
 from Backend.features import playStartSound
 
-def start():
-    eel.init("Frontend")
-    threading.Thread(target=playStartSound).start()
-    subprocess.Popen(['open', '-a', 'Safari', 'http://localhost:8000/index.html'])
-    eel.start('index.html', mode=None, host='localhost', block=True)
+eel.init("Frontend")
+subprocess.Popen(['open', '-a', 'Safari', 'http://localhost:8000/index.html'])
+eel.start('index.html', mode=None, host='localhost', block=True)
 
-if __name__ == '__main__':
-    start()
+playStartSound()
